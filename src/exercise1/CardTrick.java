419538437b163c3874b9c6d34b5bfb968e37a50b
Yesha2003 <123408109+Yesha2003@users.noreply.github.com>
2023-01-25 17:37:53 -0800

    Update CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -65,19 +65,19 @@ public class CardTrick
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Yesha Patel, but you can call me Yesha");
         System.out.println();
         
         System.out.println("My career ambitions:");
         System.out.println("-- Be more active on LinkedIn");
         System.out.println("-- Have a semester with no violations of academic integrity!");
-	System.out.println();	
+	System.out.println("-- Have a good financial stability.");	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
+        System.out.println("-- Dancing");
         System.out.println("-- Cooking");
         System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Travelling");
 
         System.out.println();
         
